82856ebdff1c04cba2f5065ef99ad75a805bf698
Danny <csk917@hotmail.com>
2021-01-01 15:43:08 +0900

    add user session persistence

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from config import Config 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,10 +12,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 print(basedir)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 #
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+#Req User to login (give Flask-login view function for logins) 
+login.login_view = 'login'
 
 if __name__ == '__main__':
 	app.run(debug=True)
